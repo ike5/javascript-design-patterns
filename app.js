35ae44a27c39d910263f1ff99ba26998f34f55b8
Ike Maldonado <maldonadoike@gmail.com>
2020-08-05 14:26:26 -0700

    computed property key efficient

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const nameKey = 'name';
 const ageKey = 'age';
 const jobKey = 'job';
 
-let person = {};
-person[nameKey] = 'Matt';
-person[ageKey] = 27;
-person[jobKey] = 'Software engineer';
+let person = {
+    [nameKey]: 'Matt',
+    [ageKey]: 27,
+    [jobKey]: 'Software engineer'
+};
+
 console.log(person);
