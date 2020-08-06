49ab001cec49b37a8c646e30e622f3f12567d815
Ike Maldonado <maldonadoike@gmail.com>
2020-08-05 18:06:12 -0700

    prototype pattern

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-// Function constructor pattern
+// prototype pattern
 
-function Person(name, age, job){
-    this.name = name;
-    this.age = age;
-    this.job = job;
-    this.sayName = function(){
-        console.log(this.name);
-    };
-}
+function Person(){}
 
-let person1 = new Person("Nicholas", 28, "Software Engineer");
-let person2 = new Person("Gred", 38, "Doctor");
+Person.prototype.name = "Nicholas";
+Person.prototype.age = 38;
+Person.prototype.job = "Software Engineer";
+Person.prototype.sayName = function(){
+    console.log(this.name);
+};
 
+let person1 = new Person();
 person1.sayName();
+
+let person2 = new Person();
 person2.sayName();
 
-console.log(person1 instanceof Object)
-console.log(person1 instanceof Person);
-console.log(person2 instanceof Object);
-console.log(person2 instanceof Person);
+console.log(person1.sayName == person2.sayName);
