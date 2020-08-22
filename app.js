023d1f8b5b7e5a279a30e2186ae8f228ad735fc0
Ike Maldonado <maldonadoike@gmail.com>
2020-08-22 09:15:43 -0700

    getter and setter 1

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-class Vehicle{
-    constructor(type){
-        this.type = type;
+class Book{
+    constructor(author){
+        this._author = author;
+    }
+
+    get writer(){
+        return this._author;
+    }
+
+    set writer(updatedAuthor){
+        this._author = updatedAuthor;
     }
 }
 
-let bus = new Vehicle('industrial');
+let greenEggs = new Book('Dr.Seuss');
 
-console.log(bus);
+console.log(greenEggs);
+console.log(greenEggs.writer);
+greenEggs.writer = 'Jodi Picoulx';
+console.log(greenEggs);
+console.log(greenEggs.writer);
