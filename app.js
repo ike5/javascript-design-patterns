0afcd01119f6b96d3738a5d329b3da4d52d53978
Ike Maldonado <maldonadoike@gmail.com>
2020-12-13 11:44:38 -0800

    method two for creating an object

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 /*
-Method one of creating an object
+Method two for creating an object
 */
 
-let newObject = {};
+// Using new Object() 
+let nObj = new Object();
 
-// Dot syntax
-newObject.things = {};
-newObject.things.obj = "pens";
-newObject.things.quantity = 5;
-newObject.things.price = 1.95;
-newObject.people = {};
-newObject.people.name = "Desi";
-newObject.people.age = 49;
-newObject.people.name = "Ike";
-newObject.people.age = 31;
+nObj.name = "Ike";
+nObj.age = 31;
+nObj.skill = "Intermediate";
+nObj.ageTimesThree = function () {
+    return x * 3;
+}
 
-// This is not the idea way to instantiate an object if you wish to put more
-// than a single value to the object's constuctor.
-
-
-let thingsInHouse = newObject.things; // note: no parenthesis since it's a key
-let peopleInHouse = newObject.people;
-// when an object is displayed a table can be used to display it
-console.table(thingsInHouse);
-console.table(peopleInHouse);
-console.table(newObject);
+console.table(nObj);
