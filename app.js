ec0cea4bc5459cc22ca6fbd661c0d6fc9c1a0718
Ike Maldonado <maldonadoike@gmail.com>
2020-12-13 13:13:26 -0800

    example of inheritance of classes

--- app.js
+++ app.js
@@ -13,11 +13,17 @@ let d = function (obj, key, value) {
 
 let geography = Object.create(null);
 defineProp(geography, "Salifornia", "Sacramento");
-console.log(geography.Salifornia);
 
 let world = Object.create(null);
 d(world, "USA", true);
 d(world, "China", true);
 d(world, "France", false);
 d(world, "Germany", false);
-console.log(world["USA"])
+
+
+// Inheritance
+let leader = Object.create(world); // inherits from world object
+d(leader, "Trump", "The United States of America");
+
+console.log(leader.Trump) // subclass property
+console.log(leader.France); // inherited property
